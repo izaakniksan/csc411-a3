504744458d1c417951d22256239ad82f8b094393
izaakniksan <izaakniksan@gmail.com>
2018-03-11 20:31:24 -0400

    part4 started

---
+++ part4.py
@@ -0,0 +1,39 @@
+import csv
+import nltk
+import os
+import random
+import collections
+import pickle
+from math import *
+import part2 as p2
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+
+if __name__ == "__main__":
+    with open('real_train.pickle', 'rb') as handle:
+        real_train = pickle.load(handle)  
+    with open('real_val.pickle', 'rb') as handle:
+        real_val = pickle.load(handle)   
+    with open('real_test.pickle', 'rb') as handle:
+        real_test = pickle.load(handle)
+    with open('fake_train.pickle', 'rb') as handle:
+        fake_train = pickle.load(handle)
+    with open('fake_val.pickle', 'rb') as handle:
+        fake_val = pickle.load(handle)
+    with open('fake_test.pickle', 'rb') as handle:
+        fake_test = pickle.load(handle)
+    with open('counts.pickle', 'rb') as handle:
+        counts = pickle.load(handle)
+    with open('real_train_lines.pickle', 'rb') as handle:
+        real_train_lines = pickle.load(handle)
+    with open('real_val_lines.pickle', 'rb') as handle:
+        real_val_lines = pickle.load(handle)
+    with open('real_test_lines.pickle', 'rb') as handle:
+        real_test_lines = pickle.load(handle)
+    with open('fake_train_lines.pickle', 'rb') as handle:
+        fake_train_lines = pickle.load(handle)
+    with open('fake_val_lines.pickle', 'rb') as handle:
+        fake_val_lines = pickle.load(handle)
+    with open('fake_test_lines.pickle', 'rb') as handle:
+        fake_test_lines = pickle.load(handle)
+        
+    
